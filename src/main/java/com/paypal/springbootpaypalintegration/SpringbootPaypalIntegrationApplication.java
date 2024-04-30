de71bef7b68d7fadb3646620c42af7b93200a9d8
Kanishka <kanishkajpanchal@outlook.com>
2024-04-30 12:51:55 +0530

    Initial commit

---
+++ src/main/java/com/paypal/springbootpaypalintegration/SpringbootPaypalIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.paypal.springbootpaypalintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPaypalIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPaypalIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paypal/springbootpaypalintegration/SpringbootPaypalIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paypal.springbootpaypalintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPaypalIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
